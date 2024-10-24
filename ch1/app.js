bf98bb173c61105ae0f9522ca3e63d298be17862
KONICCO <nico678514@gmail.com>
2024-10-24 10:30:41 +0700

    first commit

---
+++ ch1/app.js
@@ -0,0 +1,21 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        maximum: 50,
+        products: null,
+        cart: [],
+        name:'asdasd'
+    },
+    mounted: function() {
+        fetch('https://hplussport.com/api/products/order/price')
+        .then(response => response.json())
+        .then(data => {
+            this.products = data;
+        });
+    },
+    methods: {
+        addItem: function (product) {
+            this.cart.push(product);
+        }
+    }
+});
